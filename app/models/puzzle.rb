217581449c97a30cdf0765f33add9b5551ce3b63
Matt Wood <matt@mekentosj.com>
2009-08-30 19:27:35 +0100

    Added initial board setup.

--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -5,23 +5,36 @@ class Puzzle
   def initialize
     @rows = 9
     @columns = 9
-    @board = [0] * (@rows * @columns)
+    @board = [0] * @rows
+    for row in (0..@rows - 1)
+      @board[row] = [0] * @columns
+    end
 
     grid_1_numbers = (1..9).sort_by{rand}
     grid_5_numbers = (1..9).sort_by{rand}
     grid_9_numbers = (1..9).sort_by{rand}
     
-    @board[0..2] = grid_1_numbers[0..2]
-    @board[9..11] = grid_1_numbers[3..5]
-    @board[18..20] = grid_1_numbers[6..8]
+    @board[0][0..2] = grid_1_numbers[0..2]
+    @board[1][0..2] = grid_1_numbers[3..5]
+    @board[2][0..2] = grid_1_numbers[6..8]
+    
+    @board[3][3..5] = grid_5_numbers[0..2]
+    @board[4][3..5] = grid_5_numbers[3..5]
+    @board[5][3..5] = grid_5_numbers[6..8]
+    
+    @board[6][6..8] = grid_5_numbers[0..2]
+    @board[7][6..8] = grid_5_numbers[3..5]
+    @board[8][6..8] = grid_5_numbers[6..8]
   end
+  
+  
   
   def to_s
     string = spacer_row
     for row in 0..(@rows - 1)
       string += "|"
       for column in 0..(@columns - 1)
-        string += " + "
+        string += " #{@board[row][column]} "
         if (column + 2) % 3 == 1
           string += "|"
         end
