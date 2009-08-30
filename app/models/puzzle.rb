55c7b7024ebfc75cffec7f691c864dd3e844d0df
Matt Wood <matt@mekentosj.com>
2009-08-30 19:23:42 +0100

    String representation of board.

--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -1,6 +1,53 @@
 class Puzzle
   
-  def to_s
-    "hello"
+  attr_accessor :board, :rows, :columns
+  
+  def initialize
+    @rows = 9
+    @columns = 9
+    @board = [0] * (@rows * @columns)
+
+    grid_1_numbers = (1..9).sort_by{rand}
+    grid_5_numbers = (1..9).sort_by{rand}
+    grid_9_numbers = (1..9).sort_by{rand}
+    
+    @board[0..2] = grid_1_numbers[0..2]
+    @board[9..11] = grid_1_numbers[3..5]
+    @board[18..20] = grid_1_numbers[6..8]
   end
+  
+  def to_s
+    string = spacer_row
+    for row in 0..(@rows - 1)
+      string += "|"
+      for column in 0..(@columns - 1)
+        string += " + "
+        if (column + 2) % 3 == 1
+          string += "|"
+        end
+      end
+      string += "<br />"
+      if (row + 2) % 3 == 1
+         string += spacer_row
+       end
+    end
+    string
+  end
+  
+  def spacer_row
+    string = "+-"
+    for column in 0..(@columns - 1)
+      string = string + "--"
+      if (column + 2) % 3 == 1
+        string = string + "+-"
+      end
+    end
+    string.chop!
+    string += "<br />"
+  end
+  
+  def logger
+    Rails.logger
+  end
+  
 end
