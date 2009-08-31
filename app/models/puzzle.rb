dff7993b40d9a025650d384342fcb9964dcd7a79
Matt Wood <matt@mekentosj.com>
2009-08-31 09:01:04 +0100

    Added puzzle validation and generation

--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -5,6 +5,24 @@ class Puzzle
   def initialize
     @rows = 9
     @columns = 9
+
+    valid_board = false
+    while !valid_board
+      seed_board
+      reduce
+      quick_fill
+      valid_board = check_valid
+    end
+    create_puzzle
+  end
+  
+  def create_puzzle
+    76.times do
+      @board[rand(@rows)][rand(@columns)] = 0
+    end
+  end
+  
+  def seed_board
     @board = [0] * @rows
     @possibles = [0] * @rows
     for row in (0..@rows - 1)
@@ -18,40 +36,78 @@ class Puzzle
     
     assign_to_grid 0..2, 0..2, (1..9).sort_by{rand}
     assign_to_grid 3..5, 3..5, (1..9).sort_by{rand}
-    assign_to_grid 6..8, 6..8, (1..9).sort_by{rand}
-    
-    reduce
+    assign_to_grid 6..8, 6..8, (1..9).sort_by{rand} 
   end
   
   def reduce
-    for row in 0..(@rows - 1)
+    for row in 0..(@rows -1)
       for column in 0..(@columns - 1)
         if @possibles[row][column].size > 1
-          @possibles[row][column] = @possibles[row][column] - ( @board[row] | column_numbers(column) | grid_numbers(row, column) )
-          logger.debug "Reducing ROW #{row}, COLUMN #{column}:"
-          logger.debug "ROW: #{@board[row]}"
-          logger.debug "COL: #{column_numbers(column)}"
-          logger.debug "GRI: #{grid_numbers(row, column).join(', ')}"
-          logger.debug "POS: #{@possibles[row][column].join(', ')}"
-          logger.debug
+          @possibles[row][column] = @possibles[row][column] - ( row_numbers(row) | column_numbers(column) | grid_numbers(row, column) )     
         end
+        
+        if @possibles[row][column].size == 1
+          @board[row][column] = @possibles[row][column].first
+        end
+        
       end
     end
   end
   
+  def quick_fill
+    fill_rows = @rows - 1
+    for row in 0..(fill_rows)
+      for column in 0..(@columns - 1)
+        if @possibles[row][column].size > 1
+          fix_value = @possibles[row][column].rand
+          @board[row][column] = fix_value
+          @possibles[row][column] = (fix_value).to_a
+        elsif @possibles[row][column].size == 1
+          fix_value = @possibles[row][column].first
+          @board[row][column] = fix_value
+        end
+        reduce
+      end
+    end
+  end
+  
+  def check_valid
+    valid = true
+    for row in 0..(@rows - 1)
+      for column in 0..(@columns - 1)
+        if @board[row][column] == 0
+          valid = false
+        end
+      end
+    end
+    valid
+  end
+  
+  def to_html
+    self.to_s_with_line_ending "<br />"
+  end
+  
   def to_s
-    string = spacer_row
+    self.to_s_with_line_ending "\n"
+  end
+  
+  def to_s_with_line_ending(ending)
+    string = spacer_row + ending
     for row in 0..(@rows - 1)
       string += "|"
       for column in 0..(@columns - 1)
-        string += " #{@board[row][column]} "
+        to_insert = "_"
+        unless @board[row][column] == 0
+          to_insert = @board[row][column]
+        end
+        string += " #{to_insert} "
         if (column + 2) % 3 == 1
           string += "|"
         end
       end
-      string += "<br />"
+      string += ending
       if (row + 2) % 3 == 1
-         string += spacer_row
+         string += spacer_row + ending
        end
     end
     string
@@ -66,10 +122,13 @@ class Puzzle
       end
     end
     string.chop!
-    string += "<br />"
   end
   
   private
+  
+  def row_numbers(row)
+    @board[row]
+  end
   
   def column_numbers(col)
     @board.map{|r| r[col]}
